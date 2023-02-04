320a31aaf64edce40d07d34721769025b77fc19e
NazarPrysiazhnyi <2008Nazar@gmail.com>
2023-02-04 23:11:28 +0200

    Initial Commit

---
+++ MyImageCoreData/MyImageCoreDataApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyImageCoreDataApp.swift
+//  MyImageCoreData
+//
+//  Created by Nazar Prysiazhnyi on 04.02.2023.
+//
+
+import SwiftUI
+
+@main
+struct MyImageCoreDataApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
